8be7c660799f00893a2afb80f066069da8c40d00
Ariel Avila <arielaa86@gmail.com>
2021-12-28 16:37:14 +0100

    second commit

--- src/components/dropbox/DropBox.tsx
+++ src/components/dropbox/DropBox.tsx
@@ -2,15 +2,14 @@ import React, {useState} from 'react';
 import styles from "./DropBox.module.scss";
 
 export type DropBoxFolder = {
-    description: string | undefined;
+    name: string | undefined;
     files: File[];
 }
 
 export const DropBox = () => {
 
-
     const [folders, setFolders] = useState<DropBoxFolder[]>([])
-    console.log(folders)
+    const [selectedFolder, setSelectedFolder] = useState<DropBoxFolder>()
 
     const onDragOver = (ev: any) => {
         let event = ev as Event;
@@ -33,33 +32,50 @@ export const DropBox = () => {
     const addToFolder = (file: File) => {
 
         const fileExtension: string | undefined = file.name.split('.').pop();
-        const foundFolder = folders.find(folder => folder.description === fileExtension?.toLocaleUpperCase())
+        const index = folders.findIndex(folder => folder.name === fileExtension?.toLocaleUpperCase())
 
-        if (!foundFolder) {
-            const newFolder: DropBoxFolder = {description: fileExtension?.toLocaleUpperCase(), files: []}
+        if (index === -1) {
+            const newFolder: DropBoxFolder = {name: fileExtension?.toLocaleUpperCase(), files: []}
             newFolder.files = [...newFolder.files, file]
             setFolders([...folders, newFolder])
         } else {
-            const index = folders.findIndex(folder => folder.description === fileExtension?.toLocaleUpperCase())
-            const updatedFolders = [...folders]
-            updatedFolders[index].files = [...foundFolder.files, file]
-            setFolders(updatedFolders)
+            const folderCopy = [...folders]
+            folderCopy[index].files = [...folders[index].files, file]
+            setFolders(folderCopy)
         }
 
     }
 
+    const selectFolder = (folderName: string | undefined) => {
+        setSelectedFolder(folders.find(folder => folder.name === folderName))
+    }
 
     return (
+        <div className={styles.container}>
+            <div className={styles.dropArea} onDrop={onDrop} onDragOver={onDragOver}>
+                {folders.map((folder, index) =>
+                    <Folder key={`folder-${index}`} folder={folder} onClick={() => selectFolder(folder.name)}/>
+                )}
+            </div>
+            <FileList folder={selectedFolder}/>
+        </div>
+    );
+};
 
-        <div className={styles.dropArea} onDrop={onDrop} onDragOver={onDragOver}>
+const Folder = ({folder, onClick}: { folder: DropBoxFolder, onClick: (name: string | undefined) => void }) => {
+    return (
+        <div className={styles.folder} onClick={() => onClick(folder.name)}>
+            <div>{folder.name}</div>
+            <div className={styles.folderBottom}>{folder.files.length}</div>
+        </div>
+    );
+};
 
-            {folders.map((folder, index) =>
-                <div className={styles.folder}>
-                    <div key={`folder-${index}`}>{folder.description}</div>
-                    <div className={styles.folderBottom}>{folder.files.length}</div>
-                </div>
-            )}
 
+const FileList = ({folder}: { folder: DropBoxFolder | undefined }) => {
+    return (
+        <div className={styles.folderDetails}>
+                {folder?.files.map((file, index) => <div className={ index % 2 === 0 ? styles.odd : styles.even}>{file.name}</div>)}
         </div>
     );
 };
